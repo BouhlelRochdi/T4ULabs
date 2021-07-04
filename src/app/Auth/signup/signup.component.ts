c97101043bf5e38f4d7ba7fdfc263595c74c5a75
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-07-04 14:39:24 +0100

    Create SignUp forms

--- src/app/Auth/signup/signup.component.ts
+++ src/app/Auth/signup/signup.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { passwordMatch } from 'src/app/classes/password-match';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  addUser : FormGroup | null= null;
+  submited = false;
 
-  constructor() { }
+  constructor(private userService : UserService,
+    private router : Router) { }
 
   ngOnInit(): void {
+    this.addUser = new FormGroup({
+      name: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required),
+      confirmPassword: new FormControl('', Validators.required)
+    },
+    { validators: passwordMatch});
   }
 
+  validateUser() {
+    this.submited = true;
+    if (this.addUser?.invalid) {
+      return;
+    }
+    else {
+      this.userService.register(this.addUser?.value).then(res =>{
+        this.router.navigateByUrl('/auth/signin');        
+      }, err => {
+        window.alert(err)
+      })
+    }
+
+  }
 }
