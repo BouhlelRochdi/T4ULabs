553ca30062fc684b3d45c8dc05235ef97004c4cc
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-07-04 14:38:51 +0100

    Create SignIn form

--- src/app/Auth/signin/signin.component.ts
+++ src/app/Auth/signin/signin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-signin',
@@ -6,10 +9,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent implements OnInit {
+  signInForm: FormGroup | null = null;
+  submited = false;
 
-  constructor() { }
+  constructor(private userService : UserService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.signInForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', Validators.required)
+    });
   }
+  
+
+  verify(){
+    this.submited = true;
+    if (this.signInForm?.invalid) {
+      return;
+    }
+    else {
+      this.userService.signIn(this.signInForm?.value).then(res =>{
+        this.router.navigateByUrl('/upload');        
+      }, err => {
+        window.alert(err)
+      })
+  }
+}
+
+GoogleSignIn(){
+  this.userService.GoogleAuth();
+}
 
 }
